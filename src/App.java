0d644df02539822a229263447ddb195fd41a2760
winnyDiese <winnydiese@gmail.com>
2024-12-09 16:17:28 +0100

    kjhb

---
+++ src/App.java
@@ -0,0 +1,49 @@
+
+public class App{
+    public static void main(String[] args){
+
+        // Variable
+        String name = "Pascal";
+        int age = 16;
+
+
+        // Msag
+        System.out.println("Mon nom est " + name + ", j'ai " + age + " ans.");
+
+        // COndition
+        if(age >= 18){
+            System.out.println("You are an adult !");
+        }else{
+            System.out.println("You are not an adult !");
+        }
+
+        // boucle
+        int i = 0;
+        while (i < 5){
+            System.out.println("Iteraction: " +i);
+            i++;
+        }
+
+
+        // My function
+        Mara();
+    }
+
+
+
+    // fonction
+    public static void Mara(){
+        System.out.println("Wana surnom na nga !");
+    }
+    
+}
+
+
+// public class Person{
+//     String name = "Edouard";
+//     int age = 15;
+
+//     public static void Presentation(){
+//         System.out.println("I'm " +"");
+//     }
+// }
